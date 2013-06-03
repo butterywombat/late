1c7b7b9d54e0ac96c502b289e9dd5b2330bd413b
Xue Hu <butterywombat@gmail.com>
2013-06-02 23:58:15 -0700

    raild

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Late::Application.config.secret_token = '6bd2c5a1d334fd9106a80d8b50084a6791cddf37e3ef7bb9fcc94b400ffe3bc314b40e85bce4ccacd1ae40cee5ae0b1a9f71fb244b819e8f76262057c462d109'
