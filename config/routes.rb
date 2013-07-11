27a1c61d1660006c35def3768bd9f1e309e0b904
David Kariuki <dkariuki@gmail.com>
2013-07-11 13:19:14 -0700

    change root to admin dashboard for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Late::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +50,8 @@ Late::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#index'
+  root :to => 'admin/dashboard#index'
+  ActiveAdmin.routes(self)
 
   # See how all your routes lay out with "rake routes"
 
